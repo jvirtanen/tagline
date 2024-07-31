a7ab80779375e08ee88bb2f38fa3ad20e511fde2
Jussi Virtanen <jussi.virtanen@jvirtanen.org>
2024-07-31 10:00:11 +0300

    codec: Clean up 'FixDecoderException'

--- libraries/codec/src/main/java/org/jvirtanen/tagline/codec/FixDecoderException.java
+++ libraries/codec/src/main/java/org/jvirtanen/tagline/codec/FixDecoderException.java
@@ -12,36 +12,11 @@ public class FixDecoderException extends DecoderException {
 
     /**
      * Construct a new instance.
-     */
-    public FixDecoderException() {
-    }
-
-    /**
-     * Construct a new instance.
      *
      * @param message the detail message
      */
     public FixDecoderException(final String message) {
         super(message);
-    }
-
-    /**
-     * Construct a new instance.
-     *
-     * @param cause the cause
-     */
-    public FixDecoderException(final Throwable cause) {
-        super(cause);
-    }
-
-    /**
-     * Construct a new instance.
-     *
-     * @param message the detail message
-     * @param cause the cause
-     */
-    public FixDecoderException(final String message, final Throwable cause) {
-        super(message, cause);
     }
 
 }
