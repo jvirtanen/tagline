655f7d9ac168cf89f77a00c48243d109d22ea577
Jussi Virtanen <jussi.virtanen@jvirtanen.org>
2026-06-27 22:40:38 +0300

    codec: Improve 'FixTimeDecoder'
    This reduces the operation latency (with milliseconds) 50% from 6.05
    ns/op to 3.02 ns/op on Apple M1 Pro.

--- libraries/codec/src/main/java/org/jvirtanen/tagline/codec/FixTimeDecoder.java
+++ libraries/codec/src/main/java/org/jvirtanen/tagline/codec/FixTimeDecoder.java
@@ -15,36 +15,84 @@
  */
 package org.jvirtanen.tagline.codec;
 
-import static org.jvirtanen.tagline.codec.ByteArrayDecoder.*;
-import static org.jvirtanen.tagline.codec.FixConstants.*;
+import static java.nio.ByteOrder.*;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
 
 class FixTimeDecoder {
 
+    private static final VarHandle LONG = MethodHandles.byteArrayViewVarHandle(long[].class, LITTLE_ENDIAN);
+    private static final VarHandle INT = MethodHandles.byteArrayViewVarHandle(int[].class, LITTLE_ENDIAN);
+
     static void decode(final byte[] bytes, final int index, final int length, final FixTime container) {
-        if (length != 8 && length != 12)
+        if (length == 12) {
+            decodeSecond(bytes, index, container);
+            decodeMilli(bytes, index + 8, container);
+        } else if (length == 8) {
+            decodeSecond(bytes, index, container);
+            container.setMilli(0);
+        } else {
+            notTime();
+        }
+    }
+
+    private static void decodeSecond(final byte[] bytes, final int index, final FixTime container) {
+
+        // "22:05:30" = 0x30333a35303a3232
+        long bits = (long)LONG.get(bytes, index);
+
+        // ':' = 0x3a
+        if (((bits ^ 0x00003a00003a0000l) & 0x0000ff0000ff0000l) != 0)
             notTime();
 
+        // "22:05:30" = 0x0003000500000202
+        bits -= 0x30303a30303a3030l;
+
+        // 0x7f - 0x09 = 0x76
+        if (((bits | (bits + 0x7676767676767676l)) & 0x8080808080808080l) != 0)
+            notTime();
+
+        // "22:05:30" = 0x001e000005000016
+        bits = (10 * bits + (bits >>> 8)) & 0x00ff0000ff0000ffl;
+
+        int hour = (int)(bits & 0xff);
+        int minute = (int)((bits >>> 24) & 0xff);
+        int second = (int)((bits >>> 48) & 0xff);
+
         try {
-            container.setHour(decodeTwoDigits(bytes, index));
+            container.setHour(hour);
+            container.setMinute(minute);
+            container.setSecond(second);
+        } catch (IllegalArgumentException e) {
+            notTime();
+        }
+    }
 
-            if (bytes[index + 2] != ':')
-                notTime();
+    static void decodeMilli(final byte[] bytes, final int index, final FixTime container) {
 
-            container.setMinute(decodeTwoDigits(bytes, index + 3));
+        // ".250" = 0x3035322e
+        int bits = (int)INT.get(bytes, index);
 
-            if (bytes[index + 5] != ':')
-                notTime();
+        // '.' = 0x2e
+        if (((bits ^ 0x0000002e) & 0x000000ff) != 0)
+            notTime();
 
-            container.setSecond(decodeTwoDigits(bytes, index + 6));
+        // ".250" = 0x00050200
+        bits -= 0x3030302e;
 
-            if (length == 12) {
-                if (bytes[index + 8] != '.')
-                    notTime();
+        // 0x7f - 0x09 = 0x76
+        if (((bits | (bits + 0x76767676)) & 0x80808080) != 0)
+            notTime();
 
-                container.setMilli(decodeThreeDigits(bytes, index + 9));
-            } else {
-                container.setMilli(0);
-            }
+        // ".250" = 0x00001900
+        bits = (10 * bits + (bits >>> 8)) & 0xff00ff00;
+
+        int milliHigh = (int)((bits >>> 8) & 0xff);
+        int milliLow = (int)((bits >>> 24) & 0xff);
+
+        try {
+            container.setMilli(10 * milliHigh + milliLow);
         } catch (IllegalArgumentException e) {
             notTime();
         }
